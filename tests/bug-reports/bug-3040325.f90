d3068dc361ffbc695899e360d706826c4c80dfc8
rasmussn <rasmussn@kirke.lanl.gov>
2010-08-11 16:42:58 -0600

    Initial version.

---
+++ tests/bug-reports/bug-3040325.f90
@@ -0,0 +1,8 @@
+!
+! tests for bug 3040325
+!
+! Verbose output should be checked for this test
+!
+character(3) C
+end
+
---
+++ tests/bug-reports/bug-3040332.f90
@@ -0,0 +1,9 @@
+!
+! tests for bug 3040332
+!
+!
+! Verbose output should be checked for this test
+!
+   character(kind=1, len=2) :: C1
+   character(len=2, kind=1) :: C2
+end
