d584a7cab22edaee5995f7f4c2e0d64612138742
rasmussn <rasmussn@qtsoftware.apple.com>
2010-09-09 12:46:05 -0600

    Added another else if test and comments.

--- tests/bug-reports/bug-3056328.f90
+++ tests/bug-reports/bug-3056328.f90
@@ -1,6 +1,13 @@
+!
+! This file tests bug 3056328.  See also bug 3056309.  It has the same
+! properties except that there is an T_ELSEIF rather than T_ELSE T_IF.
+!
   if (a .lt. value) then
      a = 1
   else if (a .gt. value) then
      a = 3
+  else if (a .ne. work(1,to)) then
+     a = 4
   end if
+
 end
