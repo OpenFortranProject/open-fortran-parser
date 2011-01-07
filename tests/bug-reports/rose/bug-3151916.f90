cce4c75b7fdf65b25a57beafe5660b77b6bc73df
Allen Pittman <apittman@pn1169315.lanl.gov>
2011-01-07 09:56:30 -0700

    Initial version.

---
+++ tests/bug-reports/rose/bug-3151916.f90
@@ -0,0 +1,9 @@
+module newmodule
+
+public newtype
+
+type newtype
+integer :: i
+end type
+
+end module
---
+++ tests/bug-reports/rose/bug-3152988.f90
@@ -0,0 +1,17 @@
+module test_struct
+
+   type var_wrap
+      real, dimension(:,:), pointer :: ptr2d
+      integer :: sizes(3)
+   end type
+
+contains
+
+subroutine foo()
+   type(var_wrap) :: vw
+
+   vw%ptr2d(1,vw%sizes(2)) = 1
+
+end subroutine
+
+end module
