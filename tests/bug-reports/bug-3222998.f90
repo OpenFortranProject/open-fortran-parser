6888b6445ca18c26ab9e269973aef22f5aa5992b
rasmussn <rasmussn@kirkelanlgov.local>
2011-04-03 16:46:42 -0600

    Initial version

---
+++ tests/bug-reports/bug-3222998.f90
@@ -0,0 +1,15 @@
+!
+! Test for bug 3222998.  The bug was fixed by testing for ':' as well as '\'
+! in a control-edit-desc.  Both characters can terminate a format-item
+! (replace a comma and be a format-item) so both must be checked otherwise a
+! zero-length format-item token is created and there will be too many format-items.
+!
+
+1000 format(a4,:)
+
+! A more complex case.  There should be 10 format-items as a2: acts as
+! two: a2 and :
+2000 format (a4,:,'-',a2:,'-',a2,:,'-',a5) ! problem is the comma before the :
+
+end
+
