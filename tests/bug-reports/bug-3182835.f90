174d7c0227654a42f080ca2e6725c51849d32828
rasmussn <rasmussn@kirkelanlgov.local>
2011-05-08 12:16:54 -0600

    Added a more complex test and comments as to why 10 format items are created.

--- tests/bug-reports/bug-3182835.f90
+++ tests/bug-reports/bug-3182835.f90
@@ -3,4 +3,13 @@
 ! although the parser will claim success.
 !
 1000 format (a4,:)
+
+! More complex case (as it was reported to Dan from ANL):
+!
+! This should produce 10 format items as ",a2:," is really 2 edit
+! descriptors.  C1002 (R1003) states that the optional comma shall not
+! be omitted EXCEPT before or after a colon edit descriptor (10.8.3)
+!
+2000 format (a4,:,'-',a2:,'-',a2,:,'-',a5) ! problem is the comma before the :
+
 end
