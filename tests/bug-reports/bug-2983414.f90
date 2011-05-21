3a8e6e6502d1634962317fde4b563b788ce2b477
rasmussn <rasmussn@kirkelanlgov.local>
2011-05-21 08:54:59 -0600

    Initial version.

---
+++ tests/bug-reports/bug-2983414.f90
@@ -0,0 +1,6 @@
+module precision_03
+! use mpif.h
+  include 'bug-2983414.h' ! include empty file...
+! DQ (4/7/2010): "Parser failed" error for OFP (version 0.7.2) but requires empty header file
+  integer, parameter :: doubleprec=selected_real_kind(12),singleprec=selected_real_kind(6),defaultprec=kind(0.0)
+end module precision_03
