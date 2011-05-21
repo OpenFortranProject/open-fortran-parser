8da28a31a8100f4f4bbfaf31b849e7fef5039439
rasmussn <rasmussn@kirkelanlgov.local>
2011-05-21 09:53:17 -0600

    Added another test, this one with a format other than * in the print-stmt.

--- tests/bug-reports/bug-3133759.f90
+++ tests/bug-reports/bug-3133759.f90
@@ -4,12 +4,12 @@
 ! The bug is that the character type-spec in the array constructor
 ! is parsed as a derived-type-spec and not an intrinsic-type-spec.
 !
+integer :: a, b
+a = 1
+b = 3
 
-!character(2), parameter :: a = 'a'
-!character(2), dimension(2) :: list = [a, 'bb']
-!
-!print *, list
-!
 print *, [character(len=2) :: 'a', 'bb']
-!print *, [a, 'bb']
+print *, a, b
+print "(i1, i1)", a, b
+100 format(i1, i1)
 end
