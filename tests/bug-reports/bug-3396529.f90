0b0a411b4be351a5bab6adab0e790f7a7987bcaa
Scott K Warren <scott@rice.edu>
2011-09-10 18:34:52 -0500

    Add test case for bug 3396529, which seems to have been fixed previously. OFP passes it now, so I will close the bug in Tracker.

---
+++ tests/bug-reports/bug-3396529.f90
@@ -0,0 +1,9 @@
+! OFP 0.8.3 failed with the following message:
+! bug-3396529.f90 line 5:53 no viable alternative at input ''
+MODULE multi_line_breaks
+
+CHARACTER, PARAMETER :: out_1*(*) = &
+&"x", out_2*&
+&(*) = "y"
+
+END MODULE
