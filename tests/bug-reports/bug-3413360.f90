a01a378f5ab508ae7566aa9c46dcfad43ad44fbd
Craig Rasmussen <rasmussn@craig-rasmussens-macbook-2.local>
2011-09-30 00:15:07 -0600

    Initial version.

---
+++ tests/bug-reports/bug-3413360.f90
@@ -0,0 +1,8 @@
+program p 
+
+ 10200       format(/' number of separate processes to treat:',i5,/, ' number tr&
+     &eated (including multi-zone):',i5,/, ' maximum number of sigma-0''s:      &
+     &   ',i6,/, ' maximum number of temperatures:       ',i5,/, ' offset of ori&
+     &gin for interpolation:',es8.1)
+
+end program
