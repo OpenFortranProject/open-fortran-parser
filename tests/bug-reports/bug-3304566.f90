8c9ec191cebfc4f97d6c248b0ef88180c78d8a2c
rasmussn <rasmussn@kirkelanlgov.local>
2011-10-01 17:46:59 -0600

    Added another consecutive quote character checks.

--- tests/bug-reports/bug-3304566.f90
+++ tests/bug-reports/bug-3304566.f90
@@ -2,10 +2,10 @@
 ! Format statement strings containing single apostrophes fails to parse.
 !
 program test_format_strings
+050 format(' num''ber:')
 100 FORMAT ('"')
 200 FORMAT ("""")
-    print 100
-    print 200
-    print *, '"'
-    print *, """"
+300 FORMAT ('hellno')
+ 10200 format(' number of separate'' processes to treat:')
 end program
+
