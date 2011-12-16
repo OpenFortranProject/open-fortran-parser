72d2ad316284efdfde2fe8ae437ae219ea4fb242
rasmussn <rasmussn@kirkelanlgov.local>
2011-12-15 23:17:12 -0700

    Completed first usage of token file as a token source.

--- src/fortran/ofp/cparser/OFPTokenSource.c
+++ src/fortran/ofp/cparser/OFPTokenSource.c
@@ -5,9 +5,9 @@ nextToken (pANTLR3_TOKEN_SOURCE ts)
 {
    pANTLR3_COMMON_TOKEN ct = NULL;
 
-   OFP_TOKEN_SOURCE * ofp_ts = (OFP_TOKEN_SOURCE *) ts->super;
+   OFP_TOKEN_SOURCE * this = (OFP_TOKEN_SOURCE *) ts->super;
 
-   ct = (pANTLR3_COMMON_TOKEN) ofp_ts->tokens->get(ofp_ts->tokens, ofp_ts->index++);
+   ct = (pANTLR3_COMMON_TOKEN) this->tokens->get(this->tokens, this->index++);
 
    return ct;
 }
@@ -15,34 +15,69 @@ nextToken (pANTLR3_TOKEN_SOURCE ts)
 pANTLR3_TOKEN_SOURCE
 ofpTokenSourceNew(pANTLR3_UINT8 fileName, pANTLR3_VECTOR tokens)
 {
-    pOFP_TOKEN_SOURCE    ts;
-    pANTLR3_STRING       str;
+    pOFP_TOKEN_SOURCE     ts;
+    pANTLR3_STRING        str;
+    pANTLR3_COMMON_TOKEN  token;
+
+    ts  = (pOFP_TOKEN_SOURCE) ANTLR3_MALLOC(sizeof(OFP_TOKEN_SOURCE));
+    if (ts == NULL)
+       {
+          return  NULL;
+       }
 
     /* Memory for the interface structure
      */
-    //ts  = (pANTLR3_TOKEN_SOURCE) ANTLR3_MALLOC(sizeof(ANTLR3_TOKEN_SOURCE));
-    ts  = (pOFP_TOKEN_SOURCE) ANTLR3_MALLOC(sizeof(OFP_TOKEN_SOURCE));
+    ts->base = (pANTLR3_TOKEN_SOURCE) ANTLR3_CALLOC(1, sizeof(ANTLR3_TOKEN_SOURCE));
+    if (ts->base == NULL)
+       {
+          /* TODO - create and use free method */
+          ANTLR3_FREE(ts);
+          return  NULL;
+       }
 
-    ts->super    = ts;
+    /* Initialize base object
+     */
+
+    ts->base->super         = ts;
+    ts->base->nextToken     = nextToken;
+    ts->base->strFactory    = antlr3StringFactoryNew( ANTLR3_ENC_8BIT );
+
+    /* Initialise the eof token                                                                      
+     */
+    token                 = &(ts->base->eofToken);
+    antlr3SetTokenAPI  (token);
+    token->setType     (token, ANTLR3_TOKEN_EOF);
+    token->factoryMade    = ANTLR3_TRUE;      // Prevent things trying to free() it
+    token->strFactory     = NULL;
+    token->textState      = ANTLR3_TEXT_NONE;
+    token->custom         = NULL;
+    token->user1          = 0;
+    token->user2          = 0;
+    token->user3          = 0;
+
+   /* Initialize the skip token.                                                                
+    */
+    token                 = &(ts->base->skipToken);
+    antlr3SetTokenAPI  (token);
+    token->setType     (token, ANTLR3_TOKEN_INVALID);
+    token->factoryMade    = ANTLR3_TRUE;     // Prevent things trying to free() it
+    token->strFactory     = NULL;
+    token->textState      = ANTLR3_TEXT_NONE;
+    token->custom         = NULL;
+    token->user1          = 0;
+    token->user2          = 0;
+    token->user3          = 0;
+
+    /* Initialize this object
+     */
     ts->index    = 0;
     ts->tokens   = tokens;
 
-    /* make sure special pre-allocated tokens aren't freed
-     */
-    ts->eofToken.factoryMade   =  1;
-    ts->skipToken.factoryMade  =  1;
-
-    /* create string factory
-     */
-    ts->strFactory = antlr3StringFactoryNew( ANTLR3_ENC_8BIT );
-
     /* create file name
      */
-    str = ts->strFactory->newRaw(ts->strFactory);
+    str                 = ts->base->strFactory->newRaw(ts->base->strFactory);
     str->set(str, fileName);
-    ts->fileName = str;
+    ts->base->fileName  = str;
 
-    ts->nextToken = nextToken;
-
-    return (pANTLR3_TOKEN_SOURCE) ts;
+    return ts->base;
 }
