15e9818dc8f42bb5407767ccf366082d1b853b9e
rasmussn <rasmussn@kirkelanlgov.local>
2012-02-13 07:29:47 -0700

    Removed OFPTreeWalkerNew so that FTreeWalker becomes essentially a 'base' grammar.  OFPUnparserNew new to be used instead (and checker grammars when developed)

--- src/fortran/ofp/cparser/support.c
+++ src/fortran/ofp/cparser/support.c
@@ -1,10 +1,10 @@
 #include "support.h"
 #include "OFP_Type.h"
 
-pFTreeWalker OFPTreeWalkerNew (pANTLR3_COMMON_TREE_NODE_STREAM instream)
+pUnparser OFPUnparserNew (pANTLR3_COMMON_TREE_NODE_STREAM instream)
 {
    pOFP_TYPE_TABLE type_table = ofpTypeTableNew();
    ofpPushTypeTable(type_table);
 
-   return FTreeWalkerNew(instream);
+   return UnparserNew(instream);
 }
