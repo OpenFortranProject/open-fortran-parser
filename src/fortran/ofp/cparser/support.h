2c26b676de456d16ac7b6557f92c480bb2484df9
rasmussn <rasmussn@kirkelanlgov.local>
2012-02-13 07:29:34 -0700

    Removed OFPTreeWalkerNew so that FTreeWalker becomes essentially a 'base' grammar.  OFPUnparserNew new to be used instead (and checker grammars when developed)

--- src/fortran/ofp/cparser/support.h
+++ src/fortran/ofp/cparser/support.h
@@ -1,9 +1,8 @@
 #ifndef	OFP_SUPPORT_H
 #define	OFP_SUPPORT_H
 
-#include "FTreeWalker.h"
+#include "Unparser.h"
 
-pFTreeWalker OFPTreeWalkerNew (pANTLR3_COMMON_TREE_NODE_STREAM instream);
-
+pUnparser OFPUnparserNew (pANTLR3_COMMON_TREE_NODE_STREAM instream);
 
 #endif
