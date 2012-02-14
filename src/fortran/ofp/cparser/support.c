a6a58c6832a3293719fcc9bc58034ed8faa2af23
rasmussn <rasmussn@kirkegaard.lanl.gov>
2012-02-13 17:27:07 -0700

    Updated to work with checkers.

--- src/fortran/ofp/cparser/support.c
+++ src/fortran/ofp/cparser/support.c
@@ -1,10 +1,28 @@
 #include "support.h"
-#include "OFP_Type.h"
+#include "FTreeWalker.h"
 
-pUnparser OFPUnparserNew (pANTLR3_COMMON_TREE_NODE_STREAM instream)
+void print_token_text(pANTLR3_COMMON_TOKEN tok)
 {
-   pOFP_TYPE_TABLE type_table = ofpTypeTableNew();
-   ofpPushTypeTable(type_table);
+   if (tok->getType(tok) == T_EOS) {
+      printf("\n");
+   }
+   else {
+      printf("\%s", tok->getText(tok)->chars);
+   }
+}
 
-   return UnparserNew(instream);
+void print_token(pANTLR3_COMMON_TOKEN tok)
+{
+   printf("[");
+     printf("@\%d,", (int)tok->getTokenIndex(tok));
+     printf("\%d:", (int)tok->getStartIndex(tok));
+     printf("\%d=", (int)tok->getStopIndex(tok));
+     printf("'\%s',", tok->getText(tok)->chars);
+     printf("<\%d>,", (int)tok->getType(tok));
+     if (tok->getChannel(tok) > ANTLR3_TOKEN_DEFAULT_CHANNEL) {
+        printf("channel=\%d,", (int)tok->getChannel(tok));
+     }
+     printf("\%d:", (int)tok->getLine(tok));
+     printf("\%d", (int)tok->getCharPositionInLine(tok));
+   printf("]\n");
 }
