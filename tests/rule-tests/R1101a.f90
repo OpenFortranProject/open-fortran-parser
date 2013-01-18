22a3be7e63b9f6145b2dcec4bff9bafe9571ce3c
rasmussn <rasmus@uoregon.edu>
2013-01-18 14:36:34 -0800

    Initial versions separated from R1101.f03

---
+++ tests/rule-tests/R1101a.f90
@@ -0,0 +1,11 @@
+! R1101a.f90 - test of R1101 main-program
+!
+!   main-program is [ program-stmt ] [
+!      specification-part ] [ execution-part ] [
+!      internal-subprogram-part ] end-program-stmt program-stmt is
+!      PROGRAM program-name end-program-stmt is END [ PROGRAM [
+!      program-name ] ] Not tested here: specification-part,
+!      execution-part, and internal-subprogram-part.
+
+! None of the optional parts
+END
