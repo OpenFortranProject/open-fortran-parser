e37804bb4e4c1abf048b136a638e235ebb489d14
rasmussn <rasmus@uoregon.edu>
2013-01-20 18:31:00 -0800

    Initial version.

---
+++ src/fortran/ofp/CFF/tests/rule-tests/R1210.f90
@@ -0,0 +1,11 @@
+! Test external-stmt
+!      external-stmt  is  EXTERNAL [ :: ] external-name-list
+!
+! Not tested here: external-name-list
+EXTERNAL a
+EXTERNAL a, b
+EXTERNAL :: a
+EXTERNAL :: a, b
+
+END
+
