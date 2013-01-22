778c33a403d5091aee7c474fce3999a88db1294f
rasmussn <rasmus@uoregon.edu>
2013-01-22 12:18:57 -0800

    Initial version.

---
+++ src/fortran/ofp/CFF/tests/rule-tests/R1214.f90
@@ -0,0 +1,8 @@
+!! R1214 proc-decl
+!       is procedure-entity-name [ => proc-pointer-init ]
+
+! Testing proc decl list.
+!TODO-F08 PROCEDURE () foo
+!TODO-F08 PROCEDURE () truth, beauty, ugly, lies
+!TODO-F08 proc-pointer-init
+END
