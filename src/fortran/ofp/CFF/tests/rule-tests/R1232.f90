80eb888ad5bbcf2b0e85446d31d1820902ce8bfb
rasmussn <rasmus@uoregon.edu>
2013-01-22 15:31:41 -0800

    Initial version.

---
+++ src/fortran/ofp/CFF/tests/rule-tests/R1232.f90
@@ -0,0 +1,16 @@
+!! R1232 end-function-stmt
+!     is END [ FUNCTION [ function-name ] ]
+!
+FUNCTION foo1()
+  foo1 = 1.0
+END FUNCTION
+FUNCTION foo2()
+  foo2 = 2.0
+ENDFUNCTION foo2
+FUNCTION foo3()
+  foo3 = 3.0
+END FUNCTION foo3
+FUNCTION foo4()
+  foo4 = 4.0
+END
+END
