726f200851b7bf758349d3d01684497ef84631d3
rasmussn <rasmus@uoregon.edu>
2013-01-22 12:09:33 -0800

    Initial version.

---
+++ src/fortran/ofp/CFF/tests/rule-tests/R1228.f90
@@ -0,0 +1,13 @@
+!! R1228 function-stmt
+!    is   [ prefix ] FUNCTION function-name
+!                ( [ dummy-arg-name-list ] ) [ suffix ]
+
+11 FUNCTION foo()
+END FUNCTION
+FUNCTION boo(a,b)
+END FUNCTION
+INTEGER FUNCTION coo()
+END FUNCTION
+FUNCTION doo() !TODO-F08 BIND(C)
+END FUNCTION
+END
