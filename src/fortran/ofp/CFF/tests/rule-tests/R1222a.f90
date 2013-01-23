590573a83d44992bae6678b01c3c42cd356d3219
rasmussn <rasmus@uoregon.edu>
2013-01-23 10:26:45 -0800

    Initial version.

---
+++ src/fortran/ofp/CFF/tests/rule-tests/R1222a.f90
@@ -0,0 +1,9 @@
+!! R1222 actual-arg-spec
+!    is [ keyword = ] actual-arg
+
+a = foo(keyword=.not.B)
+a = foo(keyword = b)
+a = foo(keyword = udt%bar)
+!a = foo(keyword=*10)
+
+END
---
+++ src/fortran/ofp/CFF/tests/rule-tests/R1222b.f90
@@ -0,0 +1,9 @@
+!! R1222 actual-arg-spec
+!    is [ keyword = ] actual-arg
+
+!TODO-F08 a = foo(1)(keyword=.not.B)
+!TODO-F08 a = foo(1)(keyword = b)
+!TODO-F08 a = foo(1)(keyword = udt%bar)
+!TODO-F08 a = foo(1)(keyword=*10)
+
+END
