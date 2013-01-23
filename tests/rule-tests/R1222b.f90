41e53a2ab7a1c277919f78c3083bf60dac40473e
rasmussn <rasmus@uoregon.edu>
2013-01-23 10:29:41 -0800

    Replaced *.f03 by compilable *.f90 program

--- tests/rule-tests/R1222b.f90
+++ tests/rule-tests/R1222b.f90
@@ -1,6 +1,9 @@
-! R1220aal.f03 actual-arg-spec
+!! R1222 actual-arg-spec
+!    is [ keyword = ] actual-arg
+
 a = foo(1)(keyword=.not.B)
 a = foo(1)(keyword = b)
 a = foo(1)(keyword = udt%bar)
 a = foo(1)(keyword=*10)
-end
+
+END
