c049e49e0decdfefed6b3fa18f1157f75e97692f
rasmussn <rasmus@uoregon.edu>
2013-01-23 10:44:27 -0800

    a/b parts of file combined

--- tests/rule-tests/R1222.f90
+++ tests/rule-tests/R1222.f90
@@ -1,6 +1,11 @@
 !! R1222 actual-arg-spec
 !    is [ keyword = ] actual-arg
 
+a = foo(keyword=.not.B)
+a = foo(keyword = b)
+a = foo(keyword = udt%bar)
+a = foo(keyword=*10)
+
 a = foo(1)(keyword=.not.B)
 a = foo(1)(keyword = b)
 a = foo(1)(keyword = udt%bar)
