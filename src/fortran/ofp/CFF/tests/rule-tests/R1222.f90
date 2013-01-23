c337bf1a2c1039a68d6435cc48d9f3757737aa7d
rasmussn <rasmus@uoregon.edu>
2013-01-23 10:43:13 -0800

    a/b parts of file combined

--- src/fortran/ofp/CFF/tests/rule-tests/R1222.f90
+++ src/fortran/ofp/CFF/tests/rule-tests/R1222.f90
@@ -1,6 +1,11 @@
 !! R1222 actual-arg-spec
 !    is [ keyword = ] actual-arg
 
+a = foo(keyword=.not.B)
+a = foo(keyword = b)
+a = foo(keyword = udt%bar)
+!TODO-F08 a = foo(keyword=*10)
+
 !TODO-F08 a = foo(1)(keyword=.not.B)
 !TODO-F08 a = foo(1)(keyword = b)
 !TODO-F08 a = foo(1)(keyword = udt%bar)
--- src/fortran/ofp/CFF/tests/rule-tests/R1222a.f90
+++
@@ -1,9 +0,0 @@
-!! R1222 actual-arg-spec
-!    is [ keyword = ] actual-arg
-
-a = foo(keyword=.not.B)
-a = foo(keyword = b)
-a = foo(keyword = udt%bar)
-!a = foo(keyword=*10)
-
-END
