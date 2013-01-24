8ab461681b965f879049e8f4c79a9115a32c3144
rasmussn <rasmus@uoregon.edu>
2013-01-23 16:39:22 -0800

    Initial version.

---
+++ src/fortran/ofp/CFF/tests/rule-tests/R404.f90
@@ -0,0 +1,26 @@
+!! R404 intrinsic-type-spec
+!    is INTEGER [ kind-selector ]
+!    or REAL [ kind-selector ]
+!    or DOUBLE PRECISION
+!    or COMPLEX [ kind-selector ]
+!    or CHARACTER [ char-selector ]
+!    or LOGICAL [ kind-selector ]
+
+INTEGER :: a
+INTEGER(4) :: b
+REAL :: c
+REAL(4) :: d
+DOUBLE PRECISION :: e
+DOUBLEPRECISION :: f
+COMPLEX :: g
+COMPLEX(4) :: h
+DOUBLE COMPLEX :: i
+DOUBLECOMPLEX :: j
+CHARACTER :: k
+CHARACTER(1) :: l
+LOGICAL :: m
+LOGICAL(1) :: n
+INTEGER foo
+
+END
+
