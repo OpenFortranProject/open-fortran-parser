5e8cc13fdb2f2596814f6a58b5950d84eccfa0dc
rasmussn <rasmus@uoregon.edu>
2013-01-25 11:49:03 -0800

    Replaced *.f03 by compilable *.f90 program

---
+++ tests/rule-tests/R446.f90
@@ -0,0 +1,11 @@
+!! R446 proc-component-attr-spec
+!    is POINTER
+!    or PASS [ (arg-name ) ]
+!    or NOPASS
+!    or access-spec
+! 
+type truth
+!TODO-F08 contains
+!TODO-F08 	procedure, pass(beauty) :: foo
+end type truth
+end
