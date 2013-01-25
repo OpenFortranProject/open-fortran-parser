5f2ff7698acd1970f37b66f7076eb2adb84516cc
rasmussn <rasmus@uoregon.edu>
2013-01-25 07:41:16 -0800

    Putback F08 test.

--- tests/rule-tests/R437.f90
+++ tests/rule-tests/R437.f90
@@ -1,9 +1,9 @@
 !! R437 type-param-decl
 !    is type-param-name [ = scalar-int-initialization-expr ]
 
-!TODO-F08 TYPE big_deal(k, d)
-!TODO-F08   INTEGER, KIND :: k = kind(0.0), d = 13
-!TODO-F08 end type
+TYPE big_deal(k, d)
+  INTEGER, KIND :: k = kind(0.0), d = 13
+end type
 
 !! following are old tests (not sure what they test)
 !
