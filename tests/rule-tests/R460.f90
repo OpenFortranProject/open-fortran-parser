cb176b2f06854b5cf7c352355a7fe3afd32e4ac6
rasmussn <rasmus@uoregon.edu>
2013-01-25 14:31:43 -0800

    Replaced *.f03 by compilable *.f90 program

---
+++ tests/rule-tests/R460.f90
@@ -0,0 +1,12 @@
+!! R460 component-spec
+!   is [ keyword = ] component-data-source
+
+! Testing component_spec_list.
+! I haven't been able to trigger this. The following is just the code
+! for R458 type_param_spec_list.
+
+!TODO-F08 type(foo(5)) :: bar
+!TODO-F08 type(foo(dim=a)) :: abar
+!TODO-F08 type(truth(3,4,ugly=5)) :: beauty
+
+END
