afdbbbf0817fd165e6552afe9d11157cce75ce63
rasmussn <rasmus@uoregon.edu>
2013-01-25 14:39:26 -0800

    Replaced *.f03 by compilable *.f90 program

---
+++ tests/rule-tests/R462.f90
@@ -0,0 +1,9 @@
+!! R462 enum-def
+!   is enum-def-stmt
+!         enumerator-def-stmt [ enumerator-def-stmt ] ...
+!      end-enum-stmt
+!
+!TODO-F08 enum, bind(C)
+!TODO-F08 	enumerator truth, beauty, ugly, lies
+!TODO-F08 end enum 
+end
